801cf1287952d7b0f8b6d093db27c1fe72e88a74
komoliddin <komoliddinmuzaffarivich@gmail.com>
2021-12-06 21:16:37 +0500

    Mail refresh added

--- assets/index.ts
+++ assets/index.ts
@@ -1,3 +1,8 @@
 export const images = {
-    bee: require("./bee.png"),
+  bee: require("./bee.png"),
+  ellipse: require("./ellipse.png"),
+  clock: require("./clock.png"),
+  security: require("./security.png"),
+  coin: require("./coin.png"),
+  plus: require("./plus.png"),
 };
--- redux/configureStore.ts
+++ redux/configureStore.ts
@@ -4,15 +4,15 @@ import { persistStore, persistReducer } from "redux-persist";
 import { compose, createStore, Store } from "redux";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 const persistConfig = {
-    key: "root",
-    storage: AsyncStorage,
+  key: "root",
+  storage: AsyncStorage,
 };
 
 const persistedReducer = persistReducer(persistConfig, reducers);
 let composeEnhancer = __DEV__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    : compose;
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose;
 
-export let store = createStore(persistedReducer, composeEnhancer());
+export let store = createStore(persistedReducer, composeEnhancer);
 export let persistor = persistStore(store);
 export type IRoot = ReturnType<typeof store.getState>;
