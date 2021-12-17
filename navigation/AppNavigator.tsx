02b689468acd1c81519f9e186c6dd9581de7761c
KAMRONBEK <kamuranbek1998@gmail.com>
2021-12-17 23:07:35 +0500

    minor update after merge

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -5,7 +5,6 @@ import "react-native-gesture-handler";
 import { useSelector } from "react-redux";
 import { selectToken } from "../redux/slices/user/user";
 import About from "../screens/About";
-import AddTransport from "../screens/AddTransport";
 import Login from "../screens/Auth/Login/Login";
 import GetMoney from "../screens/GetMoney";
 import History from "../screens/History";
@@ -17,53 +16,63 @@ import NoInternet from "../screens/NoInternet";
 import Notifications from "../screens/Notifications";
 import OnboardingScreen from "../screens/OnboardingScreen";
 import District from "../screens/Regions/District";
-<<<<<<< HEAD
 import AddPassenger from "../screens/Taxi/AddPassenger";
-import AddLoad from "../screens/AddLoad";
-import AddTransport from "../screens/transport/AddTransport";
 import Kuriyer from "../screens/Kuriyer";
-import { useSelector } from "react-redux";
-import { selectToken } from "../redux/slices/user/user";
 import { routes } from "./routes";
-=======
->>>>>>> 0f70045adb793f75dc6038af2b0d2046da7e9c74
 import Regions from "../screens/Regions/Regions";
-import AddPassenger from "../screens/Taxi/AddPassenger";
 import EditPassenger from "../screens/Taxi/EditPassanger";
 import { navigationRef } from "./NavigationService";
-import { routes } from "./routes";
 import TabStack from "./TabStack";
+import AddTransport from "../screens/transport/AddTransport";
 
 const Stack = createStackNavigator();
 
 const AppNavigator = () => {
-  let token = useSelector(selectToken);
-  return (
-    <NavigationContainer ref={navigationRef}>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        {!token && (
-          <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} />
-        )}
-        <Stack.Screen name={routes.TAB_STACK} component={TabStack} />
-        <Stack.Screen name={routes.LOGIN} component={Login} />
-        <Stack.Screen name={routes.MY} component={MyCabinet} />
-        <Stack.Screen name={routes.MONEY} component={GetMoney} />
-        <Stack.Screen name={routes.ABOUT} component={About} />
-        <Stack.Screen name={routes.HISTORY} component={History} />
-        <Stack.Screen name={routes.NO_INTERNET} component={NoInternet} />
-        <Stack.Screen name={routes.NOTIFICATIONS} component={Notifications} />
-        <Stack.Screen name={routes.ADD_MAIL} component={AddMail} />
-        <Stack.Screen name={routes.REGION} component={Regions} />
-        <Stack.Screen name={routes.DISTRICT} component={District} />
-        <Stack.Screen name={routes.ADD_PASSENGER} component={AddPassenger} />
-        <Stack.Screen name={routes.EDIT_PASSENGER} component={EditPassenger} />
-        <Stack.Screen name={routes.ADD_LOAD} component={AddLoad} />
-        <Stack.Screen name={routes.ADD_TRANSPORT} component={AddTransport} />
-        {/*<Stack.Screen name="MyCabinetScreen" component={MyCabinet}/>*/}
-        <Stack.Screen name={routes.EDIT_LOAD} component={EditLoad} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    let token = useSelector(selectToken);
+    return (
+        <NavigationContainer ref={navigationRef}>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                {!token && (
+                    <Stack.Screen
+                        name="OnboardingScreen"
+                        component={OnboardingScreen}
+                    />
+                )}
+                <Stack.Screen name={routes.TAB_STACK} component={TabStack} />
+                <Stack.Screen name={routes.LOGIN} component={Login} />
+                <Stack.Screen name={routes.MY} component={MyCabinet} />
+                <Stack.Screen name={routes.MONEY} component={GetMoney} />
+                <Stack.Screen name={routes.ABOUT} component={About} />
+                <Stack.Screen name={routes.HISTORY} component={History} />
+                <Stack.Screen
+                    name={routes.NO_INTERNET}
+                    component={NoInternet}
+                />
+                <Stack.Screen
+                    name={routes.NOTIFICATIONS}
+                    component={Notifications}
+                />
+                <Stack.Screen name={routes.ADD_MAIL} component={AddMail} />
+                <Stack.Screen name={routes.REGION} component={Regions} />
+                <Stack.Screen name={routes.DISTRICT} component={District} />
+                <Stack.Screen
+                    name={routes.ADD_PASSENGER}
+                    component={AddPassenger}
+                />
+                <Stack.Screen
+                    name={routes.EDIT_PASSENGER}
+                    component={EditPassenger}
+                />
+                <Stack.Screen name={routes.ADD_LOAD} component={AddLoad} />
+                <Stack.Screen
+                    name={routes.ADD_TRANSPORT}
+                    component={AddTransport}
+                />
+                {/*<Stack.Screen name="MyCabinetScreen" component={MyCabinet}/>*/}
+                <Stack.Screen name={routes.EDIT_LOAD} component={EditLoad} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 };
 
 export default AppNavigator;
